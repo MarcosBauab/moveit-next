314934e457f67a5fd6a10ea381556c664ae29350
Marcos Vinicius Gasparoto Bauab <marcosbauab@gmail.com>
2021-02-25 18:55:58 -0300

    2º dia
    -Completo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,12 +4,16 @@ import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
 import styles from '../styles/pages/Home.module.css';
 
+import Head from 'next/head';
+
 export default function Home() {
   return (
     
     <div className={styles.container}>
+      <Head>
+        <title>Início | mova.se</title>
+      </Head>
       <ExperienceBar />
-
       <section>
         <div>
           <Profile />
